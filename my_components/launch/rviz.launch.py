19a156211d9a6fe14978a4bbbfd2da0b64a5e9a5
Kailash Khadka <k.khadka343@gmail.com>
2024-12-10 17:49:02 -0500

    Composition: Added rviz for visualization

---
+++ my_components/launch/rviz.launch.py
@@ -0,0 +1,27 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+# this is the function launch  system will look for
+def generate_launch_description():
+
+    ####### DATA INPUT ##########
+    package_description = "my_components"
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'rb1_rviz.rviz')
+
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir])
+
+    # create and return launch description object
+    return LaunchDescription(
+        [            
+            rviz_node
+        ]
+    )
